84a79619388de27b069dfbecf1737143cace5e05
Shukrullojon <shukrullobl@gmail.com>
2024-11-27 19:43:50 +0500

    Video link

--- database/seeders/CenterSeeder.php
+++ database/seeders/CenterSeeder.php
@@ -17,7 +17,7 @@ class CenterSeeder extends Seeder
             'text' => "Asosiy diqqatimiz farzandingizni Prezident, Al-Xorazmiy, Mirzo Ulug'bek va Ixtisoslashgan maktablarga tayyorlashga qaratilgan.
 O'quv markazimiz Toshkent shahrida o'z faoliyatini 2012-yilda boshlagan, hozirda 3 ta filiali mavjud. Filiallarimiz Chorsu, Olmazor va Shahristonda mavjud. Bizning o'quv markazda Matematika, Ona tili | Adabiyot, Ingliz tili Fizika Tarix va Huquq fanlari IT kurslarimiz ham bor.
 Darslarni 5, 10 va 15 yillik tajribaga ega. Jahon va Respublika Olimpiada g'oliblari bo'lgan ustozlar o'qitadi. Maqsadingizga \"Qorako'l Ziyo\" da ilm olish bilan erishing!",
-            'link' => "https://www.youtube.com/watch?v=PhAdo6gn06c",
+            'link' => "https://www.youtube.com/embed/PhAdo6gn06c?si=iXjJ6fZMY0HbHdVK",
             'student_count' => "4750",
         ];
         Center::create($data);
